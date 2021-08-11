0004b240f338033ebf0bce7bcf4b199cd54ec0eb
sujlee <sujlee@student.42seoul.kr>
2021-08-11 17:44:25 +0900

    [fix] reduce bundle size - fontawsome

--- vue-todolist/src/main.js
+++ vue-todolist/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './index.js';
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPlus, faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
+import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
+import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faPlus, faCheck, faTrash);
